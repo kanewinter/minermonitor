3c54017ff622ed055ec29585a2671b9d725c2c4c
Kane Lewis <nlewis@trace3.com>
2017-09-17 20:45:06 -0600

    made watcher silent

--- watcher.bash
+++ watcher.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl http://jaylin.ethosdistro.com/?json=yes -o data.json
+curl -s http://jaylin.ethosdistro.com/?json=yes -o data.json
 
-curl -u user:XC9ZEpdrjqQW -XPOST "http://35.202.37.2/elasticsearch/watcherdata/log" -d @data.json
+curl -s -u user:XC9ZEpdrjqQW -XPOST "http://35.202.37.2/elasticsearch/watcherdata/log" -d @data.json > /dev/null
 
