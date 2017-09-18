bdf27443643263c141b8316d6654e4a67231c9db
Kane Lewis <nlewis@trace3.com>
2017-09-17 20:24:48 -0600

    got watcher.bash working

--- watcher.bash
+++ watcher.bash
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-INDEX="{"index": {"_index": "meminfo", "_type": "log", "_id":0}}"
-
 curl http://jaylin.ethosdistro.com/?json=yes -o data.json
 
-echo $INDEX > watcher.json
-cat data.json >> watcher.json
+curl -u user:XC9ZEpdrjqQW -XPOST "http://35.202.37.2/elasticsearch/watcherdata/log" -d @data.json
 
-curl -u user:XC9ZEpdrjqQW -XPOST http://35.202.37.2/elasticsearch/watcherdata/log/_bulk --data-binary @watcher.json
