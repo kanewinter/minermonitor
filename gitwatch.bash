d4948d21627e5b83fe88ba585d1ec1c82971e7a2
Kane Lewis <C-Nicholas.Lewis@charter.com>
2018-01-14 21:55:57 -0700

    iiii

--- gitwatch.bash
+++ gitwatch.bash
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 C=0
-while (( $(echo "$C < 2" |bc -l) )); do
+while (( $(echo "$C < 3" |bc -l) )); do
 watch -bctg -n 10 git pull
-./nma.sh GitWatch WatchStopped "GitWatch Stopped" 2
-C=`ls -1 wagerr | wc -l`
+./nma.sh GitWatch WatchStopped "GitWatch Stopped" 2 &
+C=`ls -1 | wc -l`
 done
-./nma.sh GitWatch FilesDetected "I see $C files" 2
+./nma.sh GitWatch FilesDetected "I see $C files" 2 &
+
+while (( $(echo "$C > 3" |bc -l) )); do
+./nma.sh GitWatch FilesDetected "I see $C files" 2 &
+sleep 5m
+done
